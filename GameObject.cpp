bb86ac22da2a2b9cf984a4f1f5852a956421d624
ColeANiblett <coleaniblett@gmail.com>
2022-05-14 15:29:09 -0700

    added GameObject class

---
+++ GameObject.cpp
@@ -0,0 +1,22 @@
+#include "GameObject.h"
+
+GameObject::GameObject()
+{
+	this->name = "";
+	this->description = "";
+	this->locationDescription = "";
+	this->canBeTaken = false;
+}
+
+GameObject::GameObject(
+	std::string nameToSet,
+	std::string descriptionToSet,
+	std::string locationDescriptionToSet,
+	bool canBeTakenSetting
+)
+{
+	this->name = nameToSet;
+	this->description = descriptionToSet;
+	this->locationDescription = locationDescriptionToSet;
+	this->canBeTaken = canBeTakenSetting;
+}
---
+++ GameObject.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+class GameObject
+{
+private:
+	std::string name,
+		description,
+		locationDescription;
+	bool canBeTaken;
+public:
+	GameObject();
+	GameObject(
+		std::string nameToSet,
+		std::string descriptionToSet,
+		std::string locationDescriptionToSet,
+		bool canBeTakenSetting
+	);
+	std::string getName() { return this->name; }
+	std::string getDescription() { return this->description; }
+};
+
